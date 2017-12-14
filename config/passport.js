63a9e0e3a84b3a0675e0d7c05a8672468566a812
Kenji <kenji.cchiu@gmail.com>
2017-12-14 11:49:41 +0800

    bcrypt register & check password

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,9 @@
 const FacebookStrategy = require("passport-facebook").Strategy;
 const LocalStrategy = require("passport-local").Strategy;
-const sequelize = require('sequelize');
+const sequelize = require("sequelize");
 const User = require("../models").user;
+const bcrypt = require("bcryptjs");
+
 require("dotenv").config();
 
 module.exports = passport => {
@@ -13,29 +15,43 @@ module.exports = passport => {
         callbackURL: "http://localhost:5000/auth/facebook/callback"
       },
       function(accessToken, refreshToken, profile, cb) {
-        User.findOrCreate({ where: {emailOrId: profile.id}, defaults:{password:"",firstName: profile.displayName}})
-        .spread((user,created) =>{
-          console.log(user)
-          return cb(null, user)})
-        .catch((err)=>{return cb(err)})
+        User.findOrCreate({
+          where: { emailOrId: profile.id },
+          defaults: { password: "", firstName: profile.displayName }
+        })
+          .spread((user, created) => {
+            //console.log(user)
+            return cb(null, user);
+          })
+          .catch(err => {
+            return cb(err);
+          });
       }
     )
   );
 
   passport.use(
-    new LocalStrategy(function(username, password, done) {
-      User.findOne({ emailOrId: username }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false);
-        }
-        if (!user.verifyPassword(password)) {
-          return done(null, false);
-        }
-        return done(null, user);
-      });
+    new LocalStrategy(function(email, password, done) {
+      User.findOne({
+        where: { emailOrId: email }
+      })
+        .then(user => {
+          // check if user exist
+          if (!user) {
+            return done(null, false);
+          }
+          // check if password match hash
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false);
+            }
+          });
+        })
+        .catch(err => {
+          throw err;
+        });
     })
   );
 
