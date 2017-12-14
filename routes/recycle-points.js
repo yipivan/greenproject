e87b099a6eec61c9d5401d8134d5b1e6061e9a21
Kenji <kenji.cchiu@gmail.com>
2017-12-14 11:52:12 +0800

    comment out incomplete router

--- routes/recycle-points.js
+++ routes/recycle-points.js
@@ -8,27 +8,27 @@ router.get('/', (req,res) => {
     res.render('recycle-points/search');
 });
 
-//When search, save log (mock up)
+// //When search, save log (mock up)
 
-router.post('/search_route',(req,res)=>{
-    Search_log.create({
-        query: //query,
-        location_lat: //lat,
-        location_lng: //lng,
-        user_id: //user
-    })
+// router.post('/search_route',(req,res)=>{
+//     Search_log.create({
+//         query: //query,
+//         location_lat: //lat,
+//         location_lng: //lng,
+//         user_id: //user
+//     })
 
-    // find usage_log, if not exist, create one
-    Usage_log.findOrCreate({
-        where: {userID: user},defaults:{
-            recycle_item_qty: 0,
-            recycle_times: 0
-        }
-    })
-    .spread((usage_log,created)=>{
-        usage_log.recycle_item_qty += //req.item_qty
-        usage_log.recycle_time += 1 //
-    })
-})
+//     // find usage_log, if not exist, create one
+//     Usage_log.findOrCreate({
+//         where: {userID: user},defaults:{
+//             recycle_item_qty: 0,
+//             recycle_times: 0
+//         }
+//     })
+//     .spread((usage_log,created)=>{
+//         usage_log.recycle_item_qty += //req.item_qty
+//         usage_log.recycle_time += 1 //
+//     })
+// })
 
 module.exports = router;
