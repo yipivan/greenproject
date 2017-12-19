a2f25c1cecdca2b4f262fae0d412fcd94f17f8b1
Ivan Yip <ivan_yip@msn.com>
2017-12-19 12:13:14 +0800

    handled database insertion

--- routes/users.js
+++ routes/users.js
@@ -24,25 +24,39 @@ router.post("/register",
 );
 
 //loggined user search action (search_log + usage_log)
-router.post("/:id/search",isLoggedIn, (req, res) => {
-  
+router.post("/:id/search", isLoggedIn, (req, res) => {
+  /*
+  { 'formData[0][name]': 'waste-type',
+  'formData[0][value]': 'metal',
+  'formData[1][name]': 'quantity',
+  'formData[1][value]': '1',
+  query: 'Sheung Wan Civic Centre',
+  'latlng[]': [ '22.285954', '114.14980800000001' ] }
+  */
+  data = {
+    wastType: req.body['formData[0][value]'],
+    quantity: req.body['formData[1][value]'],
+    query: req.body.query,
+    latlng: [parseFloat(req.body['latlng[]'][0]), parseFloat(req.body['latlng[]'][1])]
+  }
+
   // create new search_log for every search.
   // authenticate by user_mailOrId == req.session.passpot.user.id == req.params.id
   User.findOne({
-    where: {emailOrId: req.session.passport.user.emailOrId}
+    where: { emailOrId: req.session.passport.user.emailOrId }
   }).then(user => {
-    if(req.params.id !== user.id){
+    if (req.params.id !== user.id) {
       console.log('Post /:id/search Not Authorised')
-      res.redirect('/login')  
+      res.redirect('/login')
     } else {
       Search_log.create({
         userId: req.session.passport.user.id,
-        query: "search_input",
-        location_lat: "location_lat",
-        location_lng: "location_lng"
+        query: data.query,
+        location_lat: data.latlng[0],
+        location_lng: data.latlng[1]
       });
     }
-  });
+  }).catch(err=> console.log(err));
 
   //create or update recycle_times data whenever confirm recycle
   Usage_log.findOrCreate({
@@ -54,9 +68,10 @@ router.post("/:id/search",isLoggedIn, (req, res) => {
       recycle_times: 0
     }
   })
-    .then(usage_log => {
-      usage_log.increment("recycle_times", { by: 1 });
-    });
+    .then(() => {
+      Usage_log.increment("recycle_times", { by: 1, where: {userId : req.session.passport.user.id} });
+    })
+    .catch(err => console.log(err));
 });
 
 router.get("/logout", (req, res) => {
@@ -65,17 +80,17 @@ router.get("/logout", (req, res) => {
   res.redirect("/");
 });
 
-  //retrieve user profile data
-router.get("/:id",isLoggedIn,(req,res)=>{
+//retrieve user profile data
+router.get("/:id", isLoggedIn, (req, res) => {
   User.findOne({
-    where:{
+    where: {
       emailOrId: req.session.passport.user.emailOrId
     }
-  }).then(user=>{
-    if(req.params.id !== user.id){
+  }).then(user => {
+    if (req.params.id !== user.id) {
       console.log('get /:id Not Authorised')
-      res.redirect('/login')  
-    } else { 
+      res.redirect('/login')
+    } else {
 
       //retrieve usage_log
       Usage_log.findOne({
@@ -98,25 +113,6 @@ router.get("/:id",isLoggedIn,(req,res)=>{
       });
     }
   })
-})
-
-
-router.post("/search", (req, res) => {
-  console.log(req.body);
-  var data = {
-    wasteType: req.body['data[0][value]'],
-    quantity: req.body['data[1][value]'],
-    location: {
-      lat: req.body['origin[lat]'],
-      lng: req.body['origin[lng]']
-    }
-  }
-
-  var searchLog = new Search_log();
-  searchLog.user_id = req.user.id;
-  searchLog.location_lat = data.location.lat;
-  searchLog.location_lng = data.location.lng;
-
 })
 
 
