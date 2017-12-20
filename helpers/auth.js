4b1e0d310a6e176b9497d48afcb91bcaaa44e717
Kenji <kenji.cchiu@gmail.com>
2017-12-20 17:42:27 +0800

    submit error flash msg

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,9 +1,10 @@
 module.exports = {
-    isLoggedIn: function(req, res, next){
-      if(req.isAuthenticated()){
-        return next();
-      }
-      console.log("user is not logged in");
-      res.redirect('/login');
+  isLoggedIn: function(req, res, next){
+    if(req.isAuthenticated()){
+      return next();
     }
+    console.log("user is not logged in");
+    res.redirect('/login');
   }
+}
+
