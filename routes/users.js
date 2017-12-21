df4a6aef0f0badc4b979371adf09d5d4cfad0be2
Ivan Yip <ivan_yip@msn.com>
2017-12-21 18:05:26 +0800

    changed database config

--- routes/users.js
+++ routes/users.js
@@ -60,26 +60,28 @@ router.post("/search", isLoggedIn, (req, res) => {
     }))
   //create or update recycle_times data whenever confirm recycle
   wasteTypes = data.wasteTypes;
-  for (wasteType of wasteTypes) {
-    promises.push(Usage_log.findOrCreate({
-      where: {
-        userId: req.user.id,
-        recycle_item_name: wasteType
-      },
-      defaults: {
-        recycle_item_name: wasteType,
-        recycle_times: 0
-      }
-    }).then(usage => {
-      usage[0]["recycle_times"] += 1;
-      usage[0].save();
-    })
-      // .then(() => {
-      //   Usage_log.increment("recycle_times", { by: 1, where: { userId: req.user.id, recycle_item_name: wasteType } });
-      // })
-      .catch(err => {
-        console.log(err)
-      }))
+  if (wasteTypes) {
+    for (wasteType of wasteTypes) {
+      promises.push(Usage_log.findOrCreate({
+        where: {
+          userId: req.user.id,
+          recycle_item_name: wasteType
+        },
+        defaults: {
+          recycle_item_name: wasteType,
+          recycle_times: 0
+        }
+      }).then(usage => {
+        usage[0]["recycle_times"] += 1;
+        usage[0].save();
+      })
+        // .then(() => {
+        //   Usage_log.increment("recycle_times", { by: 1, where: { userId: req.user.id, recycle_item_name: wasteType } });
+        // })
+        .catch(err => {
+          console.log(err)
+        }))
+    }
   }
 
   Promise.all(promises).then(() => {
@@ -106,7 +108,7 @@ router.get("/profile", isLoggedIn, (req, res) => {
     })
       .then(user => {
         if (req.user.id !== user.id) {
-          req.flash('login_error','Access Not Authorised')
+          req.flash('login_error', 'Access Not Authorised')
           res.redirect('/login')
         } else {
           //retrieve usage_log
